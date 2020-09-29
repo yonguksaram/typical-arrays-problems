51f3de74e51da7509a72170e6db5b54fa6b3b839
Stanislav Alimpov <stanislav.alimpov@vrpconsulting.com>
2020-09-29 19:56:57 +0300

    feat: implemented array methods

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  return array.reduce((min, current) => current < min ? current : min, 0);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  return array.reduce((min, current) => current > min ? current : min, 0);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  let sumOfArrayUnits = array.reduce((sum, current) => sum + current, 0);
+  return sumOfArrayUnits/array.length;
 }
